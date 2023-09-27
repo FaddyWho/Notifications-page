400467c8c4593f7dd4d7a4eec6d3d8bd76c00188
Alex <fahad9k9@gmail.com>
2023-09-27 22:27:00 +0500

    it works now!

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let mark = document.querySelector("#markread");
-
+let badge = document.querySelector("#notification-badge")
 let noti = document.querySelectorAll(".unread");
 
-// mark.addEventListener("click", e => {
-//     noti.style.backgroundColor = "white";
-// });
- 
 
 mark.addEventListener("click", e => {
     for (let i = 0; i < noti.length; i++) {
         noti[i].style.backgroundColor = "white";
     }
+    
+    badge.style.visibility = "hidden"
+
 });
  
