07e9de2612f41471c646ccbcd3031c2ea73f2b74
Alex <fahad9k9@gmail.com>
2023-09-27 15:27:55 +0500

    javascript working

---
+++ script.js
@@ -0,0 +1,8 @@
+let mark = document.querySelector("#markread");
+
+let noti = document.querySelector("#all-noti");
+
+mark.addEventListener("click", e => {
+    noti.style.backgroundColor = "green";
+});
+ 
